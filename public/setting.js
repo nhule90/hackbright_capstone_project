8b7a433b1abf61cbcb2500b5c942f46fb5040f03
nhule90 <lehoanhu90@gmail.com>
2023-08-08 00:03:37 -0500

    debug

--- public/setting.js
+++ public/setting.js
@@ -53,6 +53,7 @@ const showQuestion = () => {
         .catch(err => {
             console.log(err)
             alert('Uh oh. Your request did not work.')
+            alert(res.data)
             })
         ;
 };
